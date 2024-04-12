e137bab81e3a727c10b5d67cf2e002da50ddeea0
Your Name <you@example.com>
2024-04-12 01:57:46 +0100

    updated authentication

--- routes/get.js
+++ routes/get.js
@@ -3,27 +3,12 @@ const router = express.Router();
 const { getUserIndexOfById } = require("../utils");
 const { checkIsUser, checkToken } = require("../middleware");
 
+//MUST be removed before deployment
 router.get("/", (req, res) => {
   res.send(req.users);
 });
 
 router.get("/:id", checkToken, (req, res) => {
-  // let { id } = req.params;
-  // const { users } = req;
-
-  // id = Number(id);
-
-  // if (!id || Number.isNaN(id)) {
-  //   res.send({ status: 0, reason: "Missing or invalid ID" });
-  //   return;
-  // }
-  // const indexOf = getUserIndexOfById(users, id);
-
-  // if (indexOf === -1) {
-  //   res.send({ status: 0, reason: "User not found, check the ID" });
-  //   return;
-  // }
-
   res.send({ status: 1, user: req.authedUser });
 });
 
