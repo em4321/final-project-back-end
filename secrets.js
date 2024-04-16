d271b38010c5e6ced5cb964fdc801bc258bc7b6d
Your Name <you@example.com>
2024-04-16 16:58:05 +0100

    fixed bugs

--- secrets.js
+++ secrets.js
@@ -1,5 +1,4 @@
-// const salt = ``;
-// const apiKey = ``;
-const salt = `qw6td-a3ro4sj-9un1x2z-yp4gv`;
-const apiKey = `tqSLzqcXJGCa07Uhs3BTnZMY8XxWZE4iwLV3qb6GplXxKzYLToq4ubNRuXscl_j6_wReNE1uGpQwiSDfTajphd6TIwtYPosipSDGIhx3-6_mEUXJhBYKVPibC5XoZXYx`;
+const salt = ``;
+const apiKey = ``;
+
 module.exports = { salt, apiKey };
