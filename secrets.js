7d9e0d69591885f57207cf1877a3a8ab7977518c
Your Name <you@example.com>
2024-09-02 23:06:33 +0100

    updated

--- secrets.js
+++ secrets.js
@@ -1,4 +1,4 @@
 const salt = `qw6td-a3ro4sj-9un1x2z-yp4gv`;
-const apiKey = ``;
+const apiKey = `_6o39L1hj8F-vBTWK5QfRts34u9R7GKxhOqsrCK0kVoYRxJ8APjMBKYgbNTZox8rHE3cSxgxUjm4Ioh1gxSeQ238Ooa4JtGxraRyzXs2TA5KzVEFKNP7g2ftuqoeZnYx`;
 
 module.exports = { salt, apiKey };
